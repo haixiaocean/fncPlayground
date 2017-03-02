d472643f55ce8606e6228b5e35f6a0a634addf0c
Deep911 <deba.kgec@gmail.com>
2017-03-03 00:50:55 +0100

    Adding LSI

--- FncPart1.py
+++ FncPart1.py
@@ -44,15 +44,15 @@ bodyIds = TrainBodies['Body ID']
 bodyText = TrainBodies['articleBody'] 
 
 bodyText = np.array(bodyText)
+bodyTextOrig = bodyText
 bodyIds = np.array(bodyIds)
-
 bodyText = [line.lower().split() for line in bodyText]
-#bodyText = [[lemmatizer.lemmatize(re.sub('[^a-zA-Z0-9]', '',word)) for word in text if word not in stop] for text in bodyText]
-bodyText = [[(re.sub('[^a-zA-Z0-9]', '',word)) for word in text if word not in stop] for text in bodyText]
+bodyText = [[lemmatizer.lemmatize(re.sub('[^a-zA-Z0-9]', '',word)) for word in text if word not in stop] for text in bodyText]
+
 #create the dictionary for article bodies
 
 bodyDict = dict(zip(bodyIds,bodyText))
-
+bodyDictOrig = dict(zip(bodyIds,bodyTextOrig))
 #create the dictionary of words
 
 dictionary = corpora.Dictionary(bodyDict.values())
@@ -61,39 +61,49 @@ tfIdf = models.TfidfModel(CorporaFNCbody)
 
 FncTfidf = tfIdf[CorporaFNCbody]
 bodyDictString = dict(zip(bodyDict.keys(),range(0,len(bodyDict.values())))) 
-
+#define LSI and number of topics (k)
+lsi = models.LsiModel(FncTfidf, id2word=dictionary, num_topics=150)
+#LSI matrix for FNC body corpus
+FncLsi = lsi[FncTfidf]
 #create the stance corpora 
 
 sents = TrainStances['Headline']
+
 sents = [sent.lower().split() for sent in sents]
-#sents = [[lemmatizer.lemmatize(re.sub('[^a-zA-Z0-9]', '', w)) for w in sent if w not in stop] for sent in sents]
-sents = [[(re.sub('[^a-zA-Z0-9]', '', w)) for w in sent if w not in stop] for sent in sents]
+#sentsOrig = sents
+sents = [[lemmatizer.lemmatize(re.sub('[^a-zA-Z0-9]', '', w)) for w in sent if w not in stop] for sent in sents]
+
 
 stanceDict = dict(zip(range(0,len(np.unique(sents))),np.unique(sents)))
+#stanceDictOrig = dict(zip(range(0,len(np.unique(sentsOrig))),np.unique(sentsOrig)))
 #also define the reverse stance dictionary
 revStanceDict = { str(v): k for k, v in stanceDict.iteritems()}
 
 stanceCorpora = [dictionary.doc2bow(t) for t in stanceDict.values()]
-print ("a sample stance corpora")
-print(stanceCorpora[0])
+#print ("a sample stance corpora")
+#print(stanceCorpora[0])
 
 stanceTfidf = tfIdf[stanceCorpora]
-
+#computing the LSI for stances
+stanceLsi = lsi[stanceTfidf]
 #create a list from stance and article body combination called miniList
 
 TrainStances = np.array(TrainStances)
+TrainStances[:,0] = [a.lower().split() for a in TrainStances[:,0]]
+TrainStances[:,0] = [[lemmatizer.lemmatize(re.sub('[^a-zA-Z0-9]', '', w)) for w in sent if w not in stop] for sent in TrainStances[:,0]]
 miniList = []
 for i in range(0,len(TrainStances)):
-	miniList.append([(revStanceDict[str(sents[i])]),TrainStances[i][1],TrainStances[i][2]])
+	miniList.append([(revStanceDict[str(TrainStances[i][0])]),TrainStances[i][1],TrainStances[i][2]])
 
 
 #compute the similarities of all sentences in the document bodies 
 
-sim = similarities.docsim.MatrixSimilarity(FncTfidf)
-
+#sim = similarities.docsim.MatrixSimilarity(FncTfidf)
+#Get similarities for LSI
+sim = similarities.docsim.MatrixSimilarity(FncLsi)
 #stance to body similarities
 
-Stance2BodySim = [sim.get_similarities(stancetf) for stancetf in stanceTfidf]
+Stance2BodySim = [sim.get_similarities(stancetf) for stancetf in stanceLsi]
 
 #compute cosine similarity for the milist
 cosineSim = [[Stance2BodySim[lst[0]][bodyDictString[lst[1]]] , lst[2]] for lst in miniList]
@@ -102,12 +112,12 @@ cosineSim = [[Stance2BodySim[lst[0]][bodyDictString[lst[1]]] , lst[2]] for lst i
 
 cosineSimUnrelated = []
 for sim in cosineSim:
-    if sim[1] == 'unrelated' and sim[0] > 0.0:
+    if sim[1] == 'unrelated':
         cosineSimUnrelated.append(sim[0])
 
 cosineSimRelated = []
 for sim in cosineSim:
-    if sim[1] != 'unrelated' and sim[0] > 0.0:
+    if sim[1] != 'unrelated':
         cosineSimRelated.append(sim[0])
 
 
@@ -119,7 +129,7 @@ plt.hist(cosineSimUnrelated)
 plt.xlabel("Cosine similarities histogram comparing both related and unrelated articles")
 plt.savefig('plots/histCosineBoth.png')
 
-
+plt.clf()
 #plot cosine threshold and f1 score
 cosineThreshold = np.arange(0.0,1.0,0.01)
 Y = [getCosineThreshold(cosineSim,a) for a in cosineThreshold]
